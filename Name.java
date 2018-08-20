dd3425fb3b8e4cc267afe11cf5daa80c530d2c2f
abhishek kareer <abhishekkareer92@gmail.com>
2018-08-20 17:10:37 +0530

    day2

---
+++ Name.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+class Name
+{
+  public static void main(String [] aa)
+  {
+    
+    System.out.println("Abhishek kareer!");
+  }
+}
---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+class Hello
+{
+  public static void main(String [] aa)
+  {
+    
+    System.out.println("HELLO WORLD!");
+  }
+}
